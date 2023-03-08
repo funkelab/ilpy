843b0a82c990b39eb0b8545e37f021f142aa20df
Talley Lambert <talley.lambert@gmail.com>
2023-03-08 17:30:12 -0500

    fix non windows

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ from Cython.Build import cythonize
 from setuptools import setup
 from setuptools.extension import Extension
 
-libraries = ["libscip"]
+libraries = ["libscip"] if os.name == "nt" else ["scip"]
 compile_args = ["-O3", "-std=c++11", "-DHAVE_SCIP"]
 
 # look for various gurobi versions, which are annoyingly
 # suffixed with the version number, and wildcards don't work
 for v in ["100"]:
-    GUROBI_LIB = f"libgurobi{v}"
+    GUROBI_LIB = f"libgurobi{v}" if os.name == 'nt' else f"gurobi{v}"
     if (gurolib := util.find_library(GUROBI_LIB)) is not None:
         print("FOUND GUROBI library: ", gurolib)
         libraries.append(GUROBI_LIB)
