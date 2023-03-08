e08be6274ae814c090d73c6bb86d9d1fc09241f1
Talley Lambert <talley.lambert@gmail.com>
2023-03-07 20:06:47 -0500

    include lib on windows

--- setup.py
+++ setup.py
@@ -7,16 +7,20 @@ from setuptools import setup
 from setuptools.extension import Extension
 
 libraries = ["scip"]
+library_dirs = []
 include_dirs = ['ilpy/impl']
 compile_args = ["-O3", "-std=c++11", "-DHAVE_SCIP"]
 
-# include conda environment windows include if it exists
+# include conda environment windows include/lib if it exists
 # this will be done automatically by conda build, but is useful if someone
 # tries to build this directly with pip install in a conda environment
 if 'CONDA_PREFIX' in os.environ:
     win_include = os.path.join(os.environ['CONDA_PREFIX'], 'Library', 'include')
     if os.path.exists(win_include):
         include_dirs.append(win_include)
+    win_lib = os.path.join(os.environ['CONDA_PREFIX'], 'Library', 'lib')
+    if os.path.exists(win_lib):
+        library_dirs.append(win_lib)
     
 
 # look for various gurobi versions, which are annoyingly
@@ -52,6 +56,7 @@ setup(
                 extra_compile_args=compile_args,
                 include_dirs=include_dirs,
                 libraries=libraries,
+                library_dirs=library_dirs,
                 language='c++')
         ]),
         extras_require={
