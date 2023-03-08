d74c0e27b4500a33828c900ecd459b6644f151bb
Talley Lambert <talley.lambert@gmail.com>
2023-03-08 17:26:16 -0500

    remove lines

--- setup.py
+++ setup.py
@@ -7,17 +7,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 
 libraries = ["libscip"]
-library_dirs = []
-include_dirs = ['ilpy/impl']
 compile_args = ["-O3", "-std=c++11", "-DHAVE_SCIP"]
-
-# include conda environment windows include/lib if it exists
-# this will be done automatically by conda build, but is useful if someone
-# tries to build this directly with pip install in a conda environment
-if os.name == 'nt' and 'CONDA_PREFIX' in os.environ:
-    include_dirs.append(os.path.join(os.environ['CONDA_PREFIX'], 'Library', 'include'))
-    library_dirs.append(os.path.join(os.environ['CONDA_PREFIX'], 'Library', 'bin'))
-
 
 # look for various gurobi versions, which are annoyingly
 # suffixed with the version number, and wildcards don't work
@@ -50,9 +40,8 @@ setup(
                     'ilpy/wrapper.pyx',
                 ],
                 extra_compile_args=compile_args,
-                include_dirs=include_dirs,
+                include_dirs=['ilpy/impl'],
                 libraries=libraries,
-                library_dirs=library_dirs,
                 language='c++')
         ]),
         extras_require={
