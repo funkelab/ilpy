52de73af01d074a208b4208463e6776459e4c017
Talley Lambert <talley.lambert@gmail.com>
2023-03-09 09:18:11 -0500

    add back lib_dirs

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from dbm.ndbm import library
 import os
 
 from ctypes import util
@@ -7,12 +8,21 @@ from setuptools import setup
 from setuptools.extension import Extension
 
 libraries = ["libscip"] if os.name == "nt" else ["scip"]
+include_dirs = ["ilpy/impl"]
+library_dirs = []
 compile_args = ["-O3", "-std=c++11", "-DHAVE_SCIP"]
+
+# include conda environment windows include/lib if it exists
+# this will be done automatically by conda build, but is useful if someone
+# tries to build this directly with pip install in a conda environment
+if os.name == "nt" and "CONDA_PREFIX" in os.environ:
+    include_dirs.append(os.path.join(os.environ["CONDA_PREFIX"], "Library", "include"))
+    library_dirs.append(os.path.join(os.environ["CONDA_PREFIX"], "Library", "bin"))
 
 # look for various gurobi versions, which are annoyingly
 # suffixed with the version number, and wildcards don't work
 for v in ["100"]:
-    GUROBI_LIB = f"libgurobi{v}" if os.name == 'nt' else f"gurobi{v}"
+    GUROBI_LIB = f"libgurobi{v}" if os.name == "nt" else f"gurobi{v}"
     if (gurolib := util.find_library(GUROBI_LIB)) is not None:
         print("FOUND GUROBI library: ", gurolib)
         libraries.append(GUROBI_LIB)
@@ -23,31 +33,31 @@ else:
 
 
 setup(
-        name='ilpy',
-        version='0.2',
-        description='Python wrappers for popular MIP solvers.',
-        url='https://github.com/funkelab/ilpy',
-        author='Jan Funke',
-        author_email='funkej@janelia.hhmi.org',
-        license='MIT',
-        packages=[
-            'ilpy'
-        ],
-        ext_modules=cythonize([
+    name="ilpy",
+    version="0.2",
+    description="Python wrappers for popular MIP solvers.",
+    url="https://github.com/funkelab/ilpy",
+    author="Jan Funke",
+    author_email="funkej@janelia.hhmi.org",
+    license="MIT",
+    packages=["ilpy"],
+    ext_modules=cythonize(
+        [
             Extension(
-                'ilpy.wrapper',
+                "ilpy.wrapper",
                 sources=[
-                    'ilpy/wrapper.pyx',
+                    "ilpy/wrapper.pyx",
                 ],
                 extra_compile_args=compile_args,
-                include_dirs=['ilpy/impl'],
+                include_dirs=include_dirs,
                 libraries=libraries,
-                language='c++')
-        ]),
-        extras_require={
-          "dev": ["flake8", "pytest", "pytest-cov"],
-        },
-        package_data={
-            "ilpy": ["py.typed", "*.pyi"]
-        },
+                library_dirs=library_dirs,
+                language="c++",
+            )
+        ]
+    ),
+    extras_require={
+        "dev": ["flake8", "pytest", "pytest-cov"],
+    },
+    package_data={"ilpy": ["py.typed", "*.pyi"]},
 )
