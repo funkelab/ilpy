556f8082933c652e3576254cddc9113d1513d841
Talley Lambert <talley.lambert@gmail.com>
2023-03-11 09:33:43 -0500

    go back to star import

--- ilpy/__init__.py
+++ ilpy/__init__.py
@@ -1,26 +1,3 @@
-from .wrapper import (
-    LinearConstraint,
-    LinearConstraints,
-    LinearObjective,
-    LinearSolver,
-    Preference,
-    QuadraticObjective,
-    Relation,
-    Sense,
-    Solution,
-    VariableType,
-)
+from .wrapper import *
 
-__version__ = "0.2.1"
-__all__ = [
-    "LinearConstraint",
-    "LinearConstraints",
-    "LinearObjective",
-    "LinearSolver",
-    "Preference",
-    "QuadraticObjective",
-    "Relation",
-    "Sense",
-    "Solution",
-    "VariableType",
-]
+__version__ = "0.2.2"
