8eadac850440122233db5a6811105a1fc9d649dc
Talley Lambert <talley.lambert@gmail.com>
2023-03-11 09:53:34 -0500

    v0.2.2

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ else:
 
 setup(
         name='ilpy',
-        version='0.2.1',
+        version='0.2.2',
         description='Python wrappers for popular MIP solvers.',
         url='https://github.com/funkelab/ilpy',
         author='Jan Funke',
