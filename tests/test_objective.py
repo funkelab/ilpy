e84f2213c17f1b35e7d7f247b4a00d8ea16c1bcf
Talley Lambert <talley.lambert@gmail.com>
2023-04-10 14:30:00 -0400

    fix resize

---
+++ tests/test_objective.py
@@ -0,0 +1,12 @@
+import ilpy
+
+
+def test_resize():
+    obj = ilpy.Objective()
+    assert len(obj) == 0
+
+    obj.set_coefficient(0, 2)  # linear term (2x)
+    assert len(obj) == 1
+
+    obj.set_quadratic_coefficient(1, 1, 3)  # quadratic term (3y^2)
+    assert len(obj) == 2
