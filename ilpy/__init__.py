dd5e80c46841b6dbf5569e78656db41761c8f4ce
Talley Lambert <talley.lambert@gmail.com>
2023-05-02 20:18:25 -0400

    bump version to 0.3.0 (#31)

--- ilpy/__init__.py
+++ ilpy/__init__.py
@@ -3,7 +3,7 @@ from ._functional import solve
 from .expressions import Expression, Variable
 from .wrapper import *  # noqa: F403
 
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 __all__ = [  # noqa: F405
     "Any",
     "Binary",
