d1fb38d39936e13efaa4b3694a7b5870d41220de
Talley Lambert <talley.lambert@gmail.com>
2023-11-07 16:58:16 -0500

    test: remove breakpoints and update coverage on expressions (#42)
    * test: remove breakpoints and update coverage on expressions
    
    * add deprecation test

--- tests/test_objective.py
+++ tests/test_objective.py
@@ -1,4 +1,5 @@
 import ilpy
+import pytest
 
 
 def test_resize() -> None:
@@ -14,3 +15,8 @@ def test_resize() -> None:
     obj2 = ilpy.Objective()
     obj2.set_quadratic_coefficient(0, 0, 1)  # quadratic term (x^2)
     assert len(obj2) == 1
+
+
+def test_deprecation() -> None:
+    with pytest.warns(DeprecationWarning):
+        ilpy.LinearObjective()
