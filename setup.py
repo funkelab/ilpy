67ff753138fdffba0e49c33ee5a67e64a12a2a62
Talley Lambert <talley.lambert@gmail.com>
2024-12-08 08:59:39 -0500

    change gurobi search pattern

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+from itertools import product
 import os
 from ctypes import util
 
@@ -26,8 +27,8 @@ if os.name == "nt" and "CONDA_PREFIX" in os.environ:
 # look for various gurobi versions, which are annoyingly
 # suffixed with the version number, and wildcards don't work
 
-for v in range(100, 150):
-    GUROBI_LIB = f"libgurobi{v}" if os.name == "nt" else f"gurobi{v}"
+for v, prefix in product(range(100, 150, 10), ("lib", "")):
+    GUROBI_LIB = f"{prefix}gurobi{v}"
     if (gurolib := util.find_library(GUROBI_LIB)) is not None:
         print("FOUND GUROBI library: ", gurolib)
         libraries.append(GUROBI_LIB)
