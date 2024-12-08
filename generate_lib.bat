56c06f7669c5fa05ea60ec6940907d91530879e6
Talley Lambert <talley.lambert@gmail.com>
2024-12-08 11:50:51 -0500

    same dir

--- generate_lib.bat
+++ generate_lib.bat
@@ -1,7 +1,7 @@
-@echo off
 set DLL_PATH=%CONDA_PREFIX%\gurobi110.dll
 set DEF_NAME=gurobi110.def
 set LIB_NAME=gurobi110.lib
+set OUTPUT_DIR=%~dpDLL_PATH%
 
 :: Check if the DLL exists
 if not exist "%DLL_PATH%" (
@@ -17,11 +17,17 @@ echo LIBRARY gurobi110 > %DEF_NAME%
 echo EXPORTS >> %DEF_NAME%
 findstr /r "^[0-9A-F]" temp_exports.txt | for /f "tokens=4" %%G in ('findstr /r "^[0-9A-F]" temp_exports.txt') do echo %%G >> %DEF_NAME%
 
-:: Create .lib from the .def file
-lib /def:%DEF_NAME% /out:%LIB_NAME% /machine:x64
+:: Create .lib in the same directory as the DLL
+lib /def:%DEF_NAME% /out:%OUTPUT_DIR%%LIB_NAME% /machine:x64
 
-:: Print success message
-echo Successfully generated %LIB_NAME%
+:: Verify .lib creation
+if not exist "%OUTPUT_DIR%%LIB_NAME%" (
+    echo Error: Failed to generate %LIB_NAME% next to the DLL.
+    exit /b 1
+)
+
+:: Success message
+echo Successfully generated %OUTPUT_DIR%%LIB_NAME%
 
 :: Cleanup
 del temp_exports.txt
